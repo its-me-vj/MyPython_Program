136bcfd70ec9f2ab314956a557acd08a9d0a663d
its-me-vj <147422661+its-me-vj@users.noreply.github.com>
2025-10-04 12:05:23 +0530

    Just List Setup
    Python First Program just Started

---
+++ PythonProgram/list.py
@@ -0,0 +1,10 @@
+ls1=["mango","apple","orange"]
+print(ls1)
+ls1.append(20) #adding integer
+print(ls1)
+ls1.remove("apple") #removing by name
+print(ls1)
+ls1.remove(ls1[2])  #removing by index
+print(ls1)
+ls1.append(3.14)    # adding floating point number
+print(ls1)
